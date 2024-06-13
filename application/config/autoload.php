aec34fb3880c6bf814bb20c14eb6496c84396a31
adrielgian <adrielgian99@gmail.com>
2024-06-13 23:18:43 +0700

    Login
    Menambahkan fitur login

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'cart');
+$autoload['libraries'] = array('database', 'cart', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -132,5 +132,5 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('model_barang', 'model_invoice','model_kategori');
+$autoload['model'] = array('model_barang', 'model_invoice','model_kategori', 'model_auth');
 
