cefe480074ec2d65f76f5466bd723c11ad4c9fe6
adrielgian <adrielgian99@gmail.com>
2024-06-13 13:08:22 +0700

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -33,12 +33,8 @@
                     <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
                     <p class="card-text"><?php echo $brg->keterangan ?></p>
                     <span class="badge badge-success mb-3">Rp. <?php echo number_format($brg->harga, 0, ',', '.') ?></span><br>
-                    <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg
-                        ->id_brg, '<div class="btn btn-sm 
-                        btn-primary">Tambah ke Keranjang</div>') ?>
-                    <?php echo anchor('dashboard/detail/' .$brg->
-                        id_brg, '<div class="btn btn-sm 
-                        btn-succes">Detail</div>') ?>
+                    <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg->id_brg, '<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
+                    <?php echo anchor('dashboard/detail/' .$brg->id_brg, '<div class="btn btn-sm btn-success">Detail</div>') ?>
 
                 </div>
             </div>
