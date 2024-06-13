a2b1b97155e5a8f693faa143cbfc1dab87953ec7
Regitaceza <128021793+Regitaceza@users.noreply.github.com>
2024-06-13 13:14:52 +0700

    Update model_kategori.php
    Update model_kategori.php

--- application/models/model_kategori.php
+++ application/models/model_kategori.php
@@ -2,15 +2,15 @@
 
 class Model_kategori extends CI_Model{
     public function data_album_kpop(){
-        return $this->db->get_where("tb_barang",array('kategori' => 'album kpop'));
+        return $this->db->get_where("tb_barang",array('kategori' => 'Album'));
     }
     public function data_lightsticks(){
-        return $this->db->get_where("tb_barang",array('kategori' => 'lightsticks'));
+        return $this->db->get_where("tb_barang",array('kategori' => 'Lightsticks'));
     }
     public function data_accessories(){
-        return $this->db->get_where("tb_barang",array('kategori' => 'accessories'));
+        return $this->db->get_where("tb_barang",array('kategori' => 'Accessories'));
     }
     public function data_photocard(){
-        return $this->db->get_where("tb_barang",array('kategori' => 'photocard'));
+        return $this->db->get_where("tb_barang",array('kategori' => 'Photocard'));
     }
 }
